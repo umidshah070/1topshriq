4948c302c119d48f4eca90cd6257e4a8da325428
umidshah070 <umidjon.donaboyev@yandex.com>
2022-03-07 01:45:43 +0500

    add

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// var elHeader = document.querySelector(".site-header");
+
+// var elBtn = document.querySelector(".site-header__hamburg-menu");
+
+// elBtn.addEventListener('click', function(){
+//   elHeader.classList.toggle("site-header__nav--active")
+// });
